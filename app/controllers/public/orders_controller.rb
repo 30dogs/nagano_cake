42a942457368f3bfe81e443243b8bb092dcc248a
satoru-koyama <satoru123.fly.9.26@gmail.com>
2021-07-19 14:43:27 +0000

    [Update] public/orders/confirm.html.erb, public/orders_controller.rb

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -39,6 +39,38 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @order = Order.new()
+    @order.customer_id = current_customer.id
+    
+    if params[:delivery] = 1
+      @order.address = current_customer.address
+      @order.postcode = current_customer.postcode
+      @order.name = current_customer.name
+    elsif params[:delivery] = 2
+      delivery = Delivery.find(params[:sellect_delivery])
+      @order.address = delivery.address
+      @order.postcode = delivery.postcode
+      @order.name = delivery.name
+    elsif params[:delivery] = 3
+      @order.address = params[:address]
+      @order.postcode = params[:pastcode]
+      @order.name = params[:name]
+    end
+      
+    @order.pay_method = params[:pay_method]
+    @order.postage = 800
+    
+    @cart_item_base_price_totall = 0
+    cart_items = current_customer.cart_items
+    cart_items.each do |cart_items|
+      @cart_item_base_price_totall += cart_item.product.base_price
+    end
+    
+    @cart_item_totall = (@cart_item_base_price_totall * 1.08).floor
+    
+    @order.biling_total = @cart_item_totall + @order.postage
+    
+    @cart_items = current_customer.cart_items
   end
 
 end
