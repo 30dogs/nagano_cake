1e9b56136d01d5d91ea3beb5c7eb9fe68fe0da1b
satoru-koyama <satoru123.fly.9.26@gmail.com>
2021-07-19 11:20:46 +0000

    [Update] public/orders_controller, public/orders/new.html.erb

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,6 +1,7 @@
 class Public::OrdersController < ApplicationController
 
   def index
+    #odersテーブルのレコードをすべて取得する
     @orders = Order.all
   end
 
@@ -8,16 +9,25 @@ class Public::OrdersController < ApplicationController
   end
 
   def new
+    #customersテーブルの、ログインしている会員のレコード
     @customer = current_customer
+    #deliveriesテーブルの、ログインしている会員が登録した配送先のレコード
     deliveries = current_customer.deliveries
+    #配列をつくるために、Arrayクラスのインスタンスを生成
     @array_deliveries = Array.new
+    #eachメソッドでdeliveriesからレコードを一つずつ取り出して、繰り返し処理を行う
     deliveries.each do |delivery|
+      #レコードの値を取り出していく
       postcode = delivery.postcode
       address = delivery.address
       name = delivery.name
-      delivery_info = "#{postcode} #{address} #{name}"
+      #変数展開を使って、取り出した値を結合させて文字列にする
+      delivery_info = "〒#{postcode} #{address} #{name}"
+      #配列をつくるために、Arrayクラスのインスタンスを生成
       array_delivery = Array.new
+      #配列array_deliveryに文字列delivery_infoと数値delivery.idを要素として追加する
       array_delivery.push(delivery_info, delivery.id)
+      #配列@array_deliveriesに配列array_deliveryを要素として追加する
       @array_deliveries.push(array_delivery)
     end
   end
