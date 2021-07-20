e259ce25b71006eaaf54a0199c733a59da5bed1c
AZUSA-ANDO <awhy.m2@gmail.com>
2021-07-20 03:21:54 +0000

    miru担当分views作成

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -1,8 +1,22 @@
 class Public::DeliveriesController < ApplicationController
   def index
+    @delivery = Delivery.new
+    @deliveries = current_customer.deliveries
   end
 
+def create
+    @delivery = Delivery.new(delivery_params)
+    @delivery.customer_id = current_customer.id
+    if @delivery.save
+      redirect_to deliveries_path
+    else
+       @deliveries = current_customer.deliveries
+      render :index
+    end
+end
+
   def edit
+    @delivery = Delivery.find(params[:id])
   end
 
   def update
@@ -10,4 +24,11 @@ class Public::DeliveriesController < ApplicationController
 
   def destroy
   end
+
+  def delivery_params
+    params.require(:delivery).permit(:postcode, :address, :name)
+  end
+
+
+
 end
