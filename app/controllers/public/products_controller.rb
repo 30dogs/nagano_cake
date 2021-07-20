c628fb26d65b94d1e876bc90fc76fa93c451fe0b
ryuji-saitou <rs0623idmanagement@gmail.com>
2021-07-20 09:15:59 +0000

    [fix] admin側のviews修正完了 / 精度80%

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,6 +1,6 @@
 class Public::ProductsController < ApplicationController
   def index
-    @products = Product.page(params[:page]).reverse_order
+    @products = Product.page(params[:page]).reverse_order.per(8)
   end
 
   def show
