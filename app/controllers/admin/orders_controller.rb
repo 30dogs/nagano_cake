c8127d06b1a5ec72273a5a58a5fa6bf80f83224d
Teraiman <toshi.guitar.no1.9995@gmail.com>
2021-07-23 04:08:25 +0000

    [update]productsコントローラ

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,6 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin!
+  
   def index
     @orders = Order.page(params[:page]).reverse_order
   end
@@ -10,6 +12,25 @@ class Admin::OrdersController < ApplicationController
   def update
   end
   
-  
-  
+  private
+
+  def order_params
+    params.require(:order).permit(
+      :name,
+      :postcode,
+      :address,
+      :postage,
+      :billing_total,
+      :payment_method,
+      :status
+      )
+  end
 end
+
+  validates :name, presence: true
+  validates :postcode, presence: true
+  validates :address, presence: true
+  validates :postage, presence: true
+  validates :billing_total, presence: true
+  validates :payment_method, presence: true
+  validates :status, presence: true
